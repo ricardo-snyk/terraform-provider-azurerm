2f9b17bed3de7b6785e758acd7b8d5635f9a7bb1
Richard Park <rpark@fugue.co>
2020-10-16 00:14:34 -0400

    Removed DiffSuppressFunc in cosmosdb_account.connection_strings

--- azurerm/internal/services/cosmos/resource_arm_cosmosdb_account.go
+++ azurerm/internal/services/cosmos/resource_arm_cosmosdb_account.go
@@ -267,9 +267,6 @@ func resourceArmCosmosDbAccount() *schema.Resource {
 				Sensitive: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
-					DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-						return true
-					},
 				},
 			},
 
